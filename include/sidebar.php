4cf2a5ddad19cae0d1e315319bb45783457e5d12
hako-975 <andrifirmansaputra1@gmail.com>
2023-04-10 15:44:22 +0700

    initial commit

---
+++ include/sidebar.php
@@ -0,0 +1,10 @@
+<aside class="sidebar">
+  <h2>Tabungan</h2>
+  <ul>
+    <li><a href="dashboard.php">Dashboard</a></li>
+    <li><a href="tabungan.php">Tabungan</a></li>
+    <li><a href="siswa.php">Siswa</a></li>
+    <li><a href="user.php">User</a></li>
+    <li><a href="logout.php">Logout</a></li>
+  </ul>
+</aside>
